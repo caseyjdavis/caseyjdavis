fb688441a39f99c3c5373774ca52d2cd65c094e1
Casey Davis <casey.j.davis@gmail.com>
2025-02-22 17:04:26 -0700

    Astro website redesign initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,24 @@
+import { defineConfig } from 'astro/config';
+import svelte from '@astrojs/svelte';
+import mdx from '@astrojs/mdx';
+import remarkGfm from 'remark-gfm';
+import remarkSmartypants from 'remark-smartypants';
+import rehypeExternalLinks from 'rehype-external-links';
+import tailwind from "@astrojs/tailwind";
+
+import react from "@astrojs/react";
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://astro-blog-template.netlify.app',
+  integrations: [mdx(), svelte(), tailwind(), react()],
+  markdown: {
+    shikiConfig: {
+      theme: 'nord'
+    },
+    remarkPlugins: [remarkGfm, remarkSmartypants],
+    rehypePlugins: [[rehypeExternalLinks, {
+      target: '_blank'
+    }]]
+  }
+});

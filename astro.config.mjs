b5dc94d7f0835079feb2c55eb7ea11ef3526faa0
Casey Davis <casey.j.davis@gmail.com>
2025-02-23 17:40:56 -0700

    Tweaked index and astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://caseyjdavis.github.io',
-  base: 'dev',
   integrations: [mdx(), svelte(), tailwind(), react()],
   markdown: {
     shikiConfig: {

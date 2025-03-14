6df8155186d66b675c492831136add4f61d653ae
Casey Davis <casey.j.davis@gmail.com>
2025-03-13 19:57:44 -0600

    Added project page Vincent Vango

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,4 +1,5 @@
 
 export default new Map([
+["src/content/projects/vango/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fvango%2Findex.mdx&astroContentModuleFlag=true")],
 ["src/content/projects/markdown-resume/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fmarkdown-resume%2Findex.mdx&astroContentModuleFlag=true")]]);
 		

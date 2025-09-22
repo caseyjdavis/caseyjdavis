e1863bf1fe5f94d288a31954ef39ebd8be27f84f
Casey Davis <casey.j.davis@gmail.com>
2025-09-22 13:14:01 -0600

    Added Google Analytics code with is:inline

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,9 +1,9 @@
 
 export default new Map([
-["src/content/projects/WSL setup/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2FWSL+setup%2Findex.mdx&astroContentModuleFlag=true")],
 ["src/content/projects/dotfiles/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fdotfiles%2Findex.mdx&astroContentModuleFlag=true")],
-["src/content/projects/nv9p/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fnv9p%2Findex.mdx&astroContentModuleFlag=true")],
-["src/content/projects/psappdeploytoolkit/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fpsappdeploytoolkit%2Findex.mdx&astroContentModuleFlag=true")],
 ["src/content/projects/markdown-resume/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fmarkdown-resume%2Findex.mdx&astroContentModuleFlag=true")],
-["src/content/projects/vango/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fvango%2Findex.mdx&astroContentModuleFlag=true")]]);
+["src/content/projects/nv9p/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fnv9p%2Findex.mdx&astroContentModuleFlag=true")],
+["src/content/projects/vango/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fvango%2Findex.mdx&astroContentModuleFlag=true")],
+["src/content/projects/psappdeploytoolkit/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fpsappdeploytoolkit%2Findex.mdx&astroContentModuleFlag=true")],
+["src/content/projects/WSL setup/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2FWSL+setup%2Findex.mdx&astroContentModuleFlag=true")]]);
 		

f1899604b1bbe61eae721ba5e91fc532d9f4602a
Casey Davis <casey.j.davis@gmail.com>
2025-10-01 17:01:34 -0600

    retry 5

--- .open-next/cloudflare/next-env.mjs
+++ .open-next/cloudflare/next-env.mjs
@@ -1,3 +1,3 @@
-export const production = process.env.RESEND_API_KEY;
-export const development = process.env.RESEND_API_KEY;
+export const production = { RESEND_API_KEY:process.env.RESEND_API_KEY};
+export const development = { RESEND_API_KEY:process.env.RESEND_API_KEY};
 export const test = {};
